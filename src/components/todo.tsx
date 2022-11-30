3408e8df44b9e7a140977f5385f556e9a9b00933
sam-yng <young142001@hotmail.com>
2022-11-30 15:53:34 +1100

    pre branch off

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { addTrackers } from 'react-ga';
+import React, { useState, useRef } from "react"
+import { Helmet } from 'react-helmet'
 import '../css/global.css';
-import Plus from '../assets/images/plus.png'
+import Plus from "../assets/images/plus.png";
+import { ProList, ComList, BacList } from '../components/list'
 
 
-export function CreateTask({ addTask }) {
+function CreateTask({ addTask }) {
     const [value, setValue] = useState("");
 
     const handleSubmit = (e: { preventDefault: () => void; }) => {
@@ -29,26 +30,23 @@ export function CreateTask({ addTask }) {
                     className="h-[5vh] text-gray-500 text-center w-[6.5vw] hover:bg-gray-100 rounded-xl"
                     placeholder="Add Item"
                 />
-                <button
-                    type='submit'
-                    className='ml-2'>
-                    <img className="h-4 mb-1 inline-block" src={Plus} />
-                </button>
+                {/*
+        <button
+          type='submit'
+          className='ml-2'>
+          <img className="h-4 mb-1 inline-block" src={Plus} />
+        </button>
+        */}
             </form>
         </div>
     );
 }
 
 
-function Task({ task }) {
-    return (
-        <div>
-            {task.title}
-        </div>
-    );
-}
+
 
 function Todo() {
+
     const [tasks, setTasks] = useState([
         {
             title: "Finish todo list",
@@ -56,24 +54,89 @@ function Todo() {
         },
     ]);
 
+
     const addTask = (title: any) => {
         const newTasks = [...tasks, { title }];
         setTasks(newTasks);
     };
 
+
+
+    function Task({ task, index }) {
+
+        const removeTask = (index: number) => {
+            const newTasks = [...tasks];
+            newTasks.splice(index, 1);
+            setTasks(newTasks);
+        };
+
+        return (
+            <div
+                className='text-[18px] ml-2 mt-1 hover:cursor-pointer'>
+                {task.title}
+
+
+                <img
+                    src={Plus}
+                    className="h-4 ml-4 inline-block rotate-45"
+                    onClick={() => removeTask(index)} >
+
+                </img>
+
+            </div>
+        );
+    }
+
     return (
         <div>
             <div>
-                {tasks.map((task, index) => (
-                    <Task
-                        task={task}
-
-                    />
-                ))}
+                <CreateTask addTask={addTask} />
             </div>
+
+            <div className="flex flex-row justify-between">
+                <div className='font-interreg text-[22px] mt-4 flex-col w-full'>
+                    <h1 className='mb-4 bg-gray-100 rounded-xl w-[9vw] text-center py-2'>
+                        Backlog
+                    </h1>
+
+
+                    {
+                        tasks &&
+                        tasks.map((task, index) => (
+                            <div
+                                draggable
+                            >
+                                <Task
+                                    task={task}
+                                    index={index}
+                                    key={index}
+
+                                />
+
+                            </div>
+                        ))}
+                </div>
+
+                <div className='font-interreg text-[22px] mt-4 flex-col w-full'>
+                    <h1 className='mb-4 bg-gray-100 rounded-xl w-[9vw] text-center py-2'>
+                        In Progress
+                    </h1>
+
+                </div>
+
+                <div className='font-interreg text-[22px] mt-4 flex-col w-full'>
+                    <h1 className='mb-4 bg-gray-100 rounded-xl w-[9vw] text-center py-2'>
+                        Completed
+                    </h1>
+                </div>
+
+            </div>
+
+
 
         </div>
     );
 }
+
 
 export default Todo;
